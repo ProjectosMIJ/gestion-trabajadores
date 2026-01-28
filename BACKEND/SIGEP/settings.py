4f639f77496d98c5cf342265cc3a788b52f1737c
JFernandoB <josefernandobolivar2@gmail.com>
2026-01-28 12:34:55 -0400

    arreglo en direccion general

--- BACKEND/SIGEP/settings.py
+++ BACKEND/SIGEP/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'SIGEP.wsgi.application'
 DATABASES = {
     'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            "NAME":"SIGEP",
+            "NAME":"SIGEP2",
             "USER": "postgres",
             "PASSWORD": "admin",
             "PORT": "5432",       
