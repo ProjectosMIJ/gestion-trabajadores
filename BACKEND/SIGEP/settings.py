a87473ec896418cdfcb5b0fefbf2d1d16ba9afb3
JFernandoB <josefernandobolivar2@gmail.com>
2026-02-03 13:45:48 -0400

    actualizacion de requeriments.txt

--- BACKEND/SIGEP/settings.py
+++ BACKEND/SIGEP/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.postgresql',
             "NAME":"SIGEP",
             "USER": "postgres",
-            "PASSWORD": "Miranda29..*",
+            "PASSWORD": "admin",
             #  "HOST": "postgresdb",
              "PORT": "5432"
     }
