efc9db3c0025d388fefe0f2d7d105d026a178a85
MoisesSh <moisesmachado2006@hotmail.com>
2026-02-16 15:22:09 -0400

    validacion de regex en crear direccion general y coordinacion

--- NextJS/src/app/(protected)/dashboard/gestion-trabajadores/dependencias/crear-dependencia-direccion/schema/schemaCreateDirectionDependency.ts
+++ NextJS/src/app/(protected)/dashboard/gestion-trabajadores/dependencias/crear-dependencia-direccion/schema/schemaCreateDirectionDependency.ts
@@ -1,10 +1,17 @@
 import z from "zod";
+const regexDirectionGeneral: RegExp =
+  /^(010\d0+\d|0)$|^(010\d0\d0+\d)|^(010{3}\d0+\d)/;
+
 export const schemaCreateDirectionGeneralDp = z.object({
   dependenciaId: z.number(),
   Codigo: z
     .string({
       message: "Datos Incorrecos",
       required_error: "Datos Invalidos",
+    })
+    .trim()
+    .regex(regexDirectionGeneral, {
+      message: "Codigo No Valido Para Direccion General o Coordinación",
     })
     .min(12, {
       message: "Minimo 12 Caracteres",
